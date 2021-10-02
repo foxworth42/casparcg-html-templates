547bbfc029e1cd971d13d364539c8958e474fe93
Ed Walker <github@foxwire.org>
2021-10-01 22:51:06 -0700

    Update dependencies.

--- src/lib/TemplateHandler.spec.js
+++ src/lib/TemplateHandler.spec.js
@@ -1,181 +1,255 @@
+/**
+ * @jest-environment jsdom
+ */
 const TemplateHandler = require("./TemplateHandler");
 
 describe("CasparCG template data parser", () => {
-    it("Should successfully parse valid template data from CasparCG", () => {
-        let testHandler = new TemplateHandler();
-        let parsedTemplateData = testHandler.parseTemplateData('{"title": "Title", "subtitle": "Subtitle"}');
-        expect(parsedTemplateData['title']).toBe("Title");
-        expect(parsedTemplateData['subtitle']).toBe("Subtitle");
-    });
+  it("Should successfully parse valid template data from CasparCG", () => {
+    let testHandler = new TemplateHandler();
+    let parsedTemplateData = testHandler.parseTemplateData(
+      '{"title": "Title", "subtitle": "Subtitle"}'
+    );
+    expect(parsedTemplateData["title"]).toBe("Title");
+    expect(parsedTemplateData["subtitle"]).toBe("Subtitle");
+  });
 
-    it("Should return an empty object if template data is not valid JSON", () => {
-        let testHandler = new TemplateHandler();
-        let parsedTemplateData = testHandler.parseTemplateData("<templateData></templateData>");
-        expect(parsedTemplateData).toEqual({});
-    });
+  it("Should return an empty object if template data is not valid JSON", () => {
+    let testHandler = new TemplateHandler();
+    let parsedTemplateData = testHandler.parseTemplateData(
+      "<templateData></templateData>"
+    );
+    expect(parsedTemplateData).toEqual({});
+  });
 
-    it("Should return an empty object if template data is not passed.", () => {
-        let testHandler = new TemplateHandler();
-        let parsedTemplateData = testHandler.parseTemplateData();
-        expect(parsedTemplateData).toEqual({});
-    });
+  it("Should return an empty object if template data is not passed.", () => {
+    let testHandler = new TemplateHandler();
+    let parsedTemplateData = testHandler.parseTemplateData();
+    expect(parsedTemplateData).toEqual({});
+  });
 });
 
 describe("Template parameter handler", () => {
-    it("Should should successfully populate text for parameters that match", () => {
-        document.body.innerHTML = `
+  it("Should should successfully populate text for parameters that match", () => {
+    document.body.innerHTML = `
             <div data-param="title"></div>
             <div data-param="subtitle"></div>
         `;
-        expect(document.querySelector("[data-param='title']").textContent).toBe("");
-        expect(document.querySelector("[data-param='subtitle']").textContent).toBe("");
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "title": "Title",
-            "subtitle": "Subtitle"
-        });
-
-        expect(document.querySelector("[data-param='title']").textContent).toBe("Title");
-        expect(document.querySelector("[data-param='subtitle']").textContent).toBe("Subtitle");
+    expect(document.querySelector("[data-param='title']").textContent).toBe("");
+    expect(document.querySelector("[data-param='subtitle']").textContent).toBe(
+      ""
+    );
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters({
+      title: "Title",
+      subtitle: "Subtitle"
     });
 
-    it("Should gracefully do nothing if there are no parameters in the template data.", () => {
-        document.body.innerHTML = `
+    expect(document.querySelector("[data-param='title']").textContent).toBe(
+      "Title"
+    );
+    expect(document.querySelector("[data-param='subtitle']").textContent).toBe(
+      "Subtitle"
+    );
+  });
+
+  it("Should gracefully do nothing if there are no parameters in the template data.", () => {
+    document.body.innerHTML = `
             <div data-param="title"></div>
             <div data-param="subtitle"></div>
         `;
-        expect(document.querySelector("[data-param='title']").textContent).toBe("");
-        expect(document.querySelector("[data-param='subtitle']").textContent).toBe("");
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({});
+    expect(document.querySelector("[data-param='title']").textContent).toBe("");
+    expect(document.querySelector("[data-param='subtitle']").textContent).toBe(
+      ""
+    );
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters({});
 
-        expect(document.querySelector("[data-param='title']").textContent).toBe("");
-        expect(document.querySelector("[data-param='subtitle']").textContent).toBe("");
-    });
+    expect(document.querySelector("[data-param='title']").textContent).toBe("");
+    expect(document.querySelector("[data-param='subtitle']").textContent).toBe(
+      ""
+    );
+  });
 
-    it("Should clear text and add empty-field class if parameter is empty", () => {
-        document.body.innerHTML = `
+  it("Should clear text and add empty-field class if parameter is empty", () => {
+    document.body.innerHTML = `
             <div data-param="title">Some Title</div>
         `;
-        expect(document.querySelector("[data-param='title']").classList.contains("empty-field")).toBe(false);
-        expect(document.querySelector("[data-param='title']").textContent).toBe("Some Title");
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "title": ""
-        });
-
-        expect(document.querySelector("[data-param='title']").classList.contains("empty-field")).toBe(true);
-        expect(document.querySelector("[data-param='title']").textContent).toBe("");
+    expect(
+      document
+        .querySelector("[data-param='title']")
+        .classList.contains("empty-field")
+    ).toBe(false);
+    expect(document.querySelector("[data-param='title']").textContent).toBe(
+      "Some Title"
+    );
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters({
+      title: ""
     });
 
-    it("Should remove empty-field class if new content is not empty", () => {
-        document.body.innerHTML = `
+    expect(
+      document
+        .querySelector("[data-param='title']")
+        .classList.contains("empty-field")
+    ).toBe(true);
+    expect(document.querySelector("[data-param='title']").textContent).toBe("");
+  });
+
+  it("Should remove empty-field class if new content is not empty", () => {
+    document.body.innerHTML = `
             <div class="empty-field" data-param="title">Title</div>
         `;
-        expect(document.querySelector("[data-param='title']").classList.contains("empty-field")).toBe(true);
-        expect(document.querySelector("[data-param='title']").textContent).toBe("Title");
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "title": "New Title"
-        });
-
-        expect(document.querySelector("[data-param='title']").classList.contains("empty-field")).toBe(false);
-        expect(document.querySelector("[data-param='title']").textContent).toBe("New Title");
+    expect(
+      document
+        .querySelector("[data-param='title']")
+        .classList.contains("empty-field")
+    ).toBe(true);
+    expect(document.querySelector("[data-param='title']").textContent).toBe(
+      "Title"
+    );
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters({
+      title: "New Title"
     });
 
-    it("Should leave parent element alone by default if parameter is empty", () => {
-        document.body.innerHTML = `
+    expect(
+      document
+        .querySelector("[data-param='title']")
+        .classList.contains("empty-field")
+    ).toBe(false);
+    expect(document.querySelector("[data-param='title']").textContent).toBe(
+      "New Title"
+    );
+  });
+
+  it("Should leave parent element alone by default if parameter is empty", () => {
+    document.body.innerHTML = `
             <div id="titleParent">
                 <div data-param="title">Title</div>
             </div>
         `;
-        expect(document.querySelector("#titleParent").classList.contains("empty-wrapper")).toBe(false);
+    expect(
+      document.querySelector("#titleParent").classList.contains("empty-wrapper")
+    ).toBe(false);
 
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "title": ""
-        });
-
-        expect(document.querySelector("#titleParent").classList.contains("empty-wrapper")).toBe(false);
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters({
+      title: ""
     });
 
-    it("Should leave parent element alone if parameter is empty and hideParentOnEmpty flag is false", () => {
-        document.body.innerHTML = `
+    expect(
+      document.querySelector("#titleParent").classList.contains("empty-wrapper")
+    ).toBe(false);
+  });
+
+  it("Should leave parent element alone if parameter is empty and hideParentOnEmpty flag is false", () => {
+    document.body.innerHTML = `
             <div id="titleParent">
                 <div data-param="title">Title</div>
             </div>
         `;
-        expect(document.querySelector("#titleParent").classList.contains("empty-wrapper")).toBe(false);
+    expect(
+      document.querySelector("#titleParent").classList.contains("empty-wrapper")
+    ).toBe(false);
 
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "title": ""
-        }, false);
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters(
+      {
+        title: ""
+      },
+      false
+    );
 
-        expect(document.querySelector("#titleParent").classList.contains("empty-wrapper")).toBe(false);
-    });
+    expect(
+      document.querySelector("#titleParent").classList.contains("empty-wrapper")
+    ).toBe(false);
+  });
 
-    it("Should add empty-wrapper class to parent element if parameter is empty and hideParentOnEmpty flag is true", () => {
-        document.body.innerHTML = `
+  it("Should add empty-wrapper class to parent element if parameter is empty and hideParentOnEmpty flag is true", () => {
+    document.body.innerHTML = `
             <div id="titleParent">
                 <div data-param="title">Title</div>
             </div>
         `;
-        expect(document.querySelector("#titleParent").classList.contains("empty-wrapper")).toBe(false);
+    expect(
+      document.querySelector("#titleParent").classList.contains("empty-wrapper")
+    ).toBe(false);
 
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "title": ""
-        }, true);
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters(
+      {
+        title: ""
+      },
+      true
+    );
 
-        expect(document.querySelector("#titleParent").classList.contains("empty-wrapper")).toBe(true);
-    });
+    expect(
+      document.querySelector("#titleParent").classList.contains("empty-wrapper")
+    ).toBe(true);
+  });
 
-    it("Should remove empty-wrapper class from parent element if parameter is populated and hideParentOnEmpty flag is true", () => {
-        document.body.innerHTML = `
+  it("Should remove empty-wrapper class from parent element if parameter is populated and hideParentOnEmpty flag is true", () => {
+    document.body.innerHTML = `
             <div id="titleParent" class="empty-wrapper">
                 <div data-param="title"></div>
             </div>
         `;
-        expect(document.querySelector("#titleParent").classList.contains("empty-wrapper")).toBe(true);
+    expect(
+      document.querySelector("#titleParent").classList.contains("empty-wrapper")
+    ).toBe(true);
 
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "title": "New Title"
-        }, true);
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters(
+      {
+        title: "New Title"
+      },
+      true
+    );
 
-        expect(document.querySelector("#titleParent").classList.contains("empty-wrapper")).toBe(false);
-    });
+    expect(
+      document.querySelector("#titleParent").classList.contains("empty-wrapper")
+    ).toBe(false);
+  });
 
-    it("Should handle updating images in templates", () => {
-        document.body.innerHTML = `
+  it("Should handle updating images in templates", () => {
+    document.body.innerHTML = `
             <div id="imageWrapper">
                 <img data-param="logo" src="logo.png">
             </div>
         `;
-        expect(document.querySelector("[data-param='logo']").src).toContain("logo.png");
+    expect(document.querySelector("[data-param='logo']").src).toContain(
+      "logo.png"
+    );
 
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "logo": "logo2.png"
-        }, true);
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters(
+      {
+        logo: "logo2.png"
+      },
+      true
+    );
 
-        expect(document.querySelector("[data-param='logo']").src).toContain("logo2.png");
-    });
+    expect(document.querySelector("[data-param='logo']").src).toContain(
+      "logo2.png"
+    );
+  });
 
-    it("Should gracefully handle not finding any elements", () => {
-        let markup = `
+  it("Should gracefully handle not finding any elements", () => {
+    let markup = `
             <div id="titleParent">
                 <div>Title</div>
             </div>
         `;
-        document.body.innerHTML = markup;
+    document.body.innerHTML = markup;
 
-        let testHandler = new TemplateHandler();
-        testHandler.fillParameters({
-            "title": ""
-        }, false);
+    let testHandler = new TemplateHandler();
+    testHandler.fillParameters(
+      {
+        title: ""
+      },
+      false
+    );
 
-        expect(document.body.innerHTML).toBe(markup);
-    });
+    expect(document.body.innerHTML).toBe(markup);
+  });
 });
